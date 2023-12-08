45db2acfb053f1894089d4f05cf4b42bc661af9e
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-12-08 20:59:55 +0100

    sawabt sift message btaswira w tafichiha f sidebar chat

--- chat_app/src/components/Input.jsx
+++ chat_app/src/components/Input.jsx
@@ -6,102 +6,129 @@ import { AuthContext } from '../context/AuthContext';
 import { doc, updateDoc, arrayUnion, arrayRemove, Timestamp, serverTimestamp } from "firebase/firestore";
 import { v4 as uuid } from "uuid";
 import { storage } from '../Firebase';
-import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import {db} from '../Firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../Firebase'
 
 
 const Input = () => {
-  const [text,setText]=useState('');
-  const [img,setImg]=useState(null);
+  const [text, setText] = useState('');
+  const [img, setImg] = useState(null);
 
-  const {currentUser}=useContext(AuthContext);
-  const {data}=useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
 
 
   //send texts between two users
-  const handelSend=async ()=>{
+  const handelSend = async () => {
 
-  if (img) {
-    const storageRef = ref(storage, uuid());
-           //ref(storage, uuid()): This creates a reference to a specific location in Firebase Storage.
-          // The ref function takes the storage instance and a path or identifier for the specific file.
-         // In this case, it seems to be using a UUID as the file identifier.
-
-    const uploadTask = uploadBytesResumable(storageRef, img);
-      //       //uploadBytesResumable: This function is part of the Firebase Storage SDK. 
-  //       // It creates an upload task for the specified file bytes (img) to the specified storage reference (storageRef).
-  //       // The "Resumable" part implies that the upload can be paused and resumed, which can be useful for large files or situations where the 
-  //       // network connection may be unreliable.
-
-    uploadTask.on(
-      (error) => {
-        //TODO:Handle Error
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-          await updateDoc(doc(db, "chats", data.chatId), {
-            messages: arrayUnion({
-              id: uuid(),
-              text,
-              senderId: currentUser.uid,
-              date: Timestamp.now(),
-              img: downloadURL,
-            }),
-          });
-        });
-      }
-    );
-  }
-      else{
-        await updateDoc(doc(db, "chats", data.chatId), {
-          messages: arrayUnion({
-            id: uuid(),
-            text,
-            senderId: currentUser.uid,
-            date: Timestamp.now(),
-          }),
-        });
-        // update userchat take last date and last message
-        await updateDoc(doc(db, "userChats", currentUser.uid), {
-          [data.chatId + ".lastMessage"]: {
-            text,
-          },
-          [data.chatId + ".date"]: serverTimestamp(),
-        });
+    ///
+    function formatCurrentDate() {
+      const today = new Date();
+      const formattedDate = [
+        today.getDate(), // Day of the month
+        today.toLocaleString('en-GB', { month: 'long' }),  // Full month name
+        today.getFullYear(), // Year
+        today.toLocaleString('en-US', { weekday: 'long' }), // Full day name
+        today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) // Time
+      ];
     
-        await updateDoc(doc(db, "userChats", data.user.uid), {
-          [data.chatId + ".lastMessage"]: {
-            text,
-          },
-          [data.chatId + ".date"]: serverTimestamp(),
-        });
+      return formattedDate;
+    }
+    
+    const currentDateArray = formatCurrentDate();
 
-      }
+    
 
-      setImg(null);
-      setText("");
+    if (img) {
+      const storageRef = ref(storage, uuid());
+      //ref(storage, uuid()): This creates a reference to a specific location in Firebase Storage.
+      // The ref function takes the storage instance and a path or identifier for the specific file.
+      // In this case, it seems to be using a UUID as the file identifier.
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+      //       //uploadBytesResumable: This function is part of the Firebase Storage SDK. 
+      //       // It creates an upload task for the specified file bytes (img) to the specified storage reference (storageRef).
+      //       // The "Resumable" part implies that the upload can be paused and resumed, which can be useful for large files or situations where the 
+      //       // network connection may be unreliable.
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+              // Get the date of the message
+
+
+            // Format the date as "Monday at 12:36"
+
+
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: currentDateArray,
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    }
+    else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date:currentDateArray,
+        }),
+      });
+      // update userchat take last date and last message
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [data.chatId + ".lastMessage"]: {
+          text,
+        },
+        [data.chatId + ".date"]: serverTimestamp(),
+      });
+
+      await updateDoc(doc(db, "userChats", data.user.uid), {
+        [data.chatId + ".lastMessage"]: {
+          text,
+        },
+        [data.chatId + ".date"]: serverTimestamp(),
+      });
+    }
+
+    setImg(null);
+    setText("");
   }
   return (
     <div className="input">
-    <input
-      type="text"
-      placeholder="Type something..."  onChange={(e)=>{setText(e.target.value)}}
-      value={text}
-    />
-    <div className="send">
-      <img src={Attach} alt="" />
       <input
-        type="file"
-        style={{ display: "none" }}
-        id="file" onChange={(e)=> setImg(e.target.files[0])}
-      
+        type="text"
+        placeholder="Type something..."   onChange={(e) => setText(e.target.value)} onKeyDown={(e)=>{
+          if (e.key === 'Enter') {
+            handelSend();
+          }
+        }}
+        value={text}
       />
-      <label htmlFor="file">
-        <img src={Img} alt="" />
-      </label>
-      <button onClick={handelSend} >Send</button>
-    </div>
-  </div>  )
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file" onChange={(e) => setImg(e.target.files[0])}
+
+        />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button onClick={handelSend} >Send</button>
+      </div>
+    </div>)
 }
 
 export default Input
--- chat_app/src/components/Message.jsx
+++ chat_app/src/components/Message.jsx
@@ -1,21 +1,80 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import profileImg from '../img/anass.png'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
+import { ref } from 'firebase/storage'
 
 const Message = ({message}) => {
   const {currentUser}=useContext(AuthContext)
   const {data}=useContext(ChatContext);
+  const ref = useRef();
+  useEffect(()=>{
+    ref.current?.scrollIntoView({ behavior: "smooth" });  // tscoller ltaht mnin tbadal tsafat message wla yji message
+  },[message])
 
-  const date = new Date(message.date.seconds * 1000 + message.date.nanoseconds / 1e6);
-  const hours = date.getUTCHours()+1;
-  const minutes = date.getUTCMinutes();
-  // Format the time
-  const time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+  function getDaysOfMonthForThisWeek() {
+    const today = new Date();
+    const currentDay = today.getDate();
+    const currentDayIndex = today.getDay(); // Get the current day index (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+    const startDay = currentDay - currentDayIndex + 1; // Calculate the start day of the week
+    const endDay = startDay + 6; // Calculate the end day of the week
+  
+    // Ensure startDay is not less than 1
+    const correctedStartDay = startDay > 0 ? startDay : 1;
+  
+    const daysOfMonthForThisWeek = [];
+    for (let i = correctedStartDay; i <= endDay; i++) {
+      daysOfMonthForThisWeek.push(i);
+    }
+  
+    return daysOfMonthForThisWeek;
+  }
+  
+  const daysOfMonthForThisWeek = getDaysOfMonthForThisWeek();
 
-  console.log('message');
+//  jbad had nhar mn chhar
+function getDayOfMonth() {
+  const today = new Date();
+  const dayOfMonth = today.getDate();
+  return dayOfMonth;
+}
+const currentDayOfMonth = getDayOfMonth();
+
+// jbad nhar lawal mn had simana
+
+function getFirstDayOfCurrentWeek() {
+  const today = new Date();
+  const currentDayIndex = today.getDay() -1;
+  const firstDay = new Date(today);
+  firstDay.setDate(today.getDate()-currentDayIndex);
+  // console.log(firstDay)  // monday-lundi
+  return firstDay;
+}
+const firstDayOfCurrentWeek = getFirstDayOfCurrentWeek();
+
+
+// Given date [6, 'December', 2023, 'Thursday', '12:41 PM']
+ // Note: Months are zero-based in JavaScript, so 11 represents December
+
+ //condition of typing timing of mesaages
+const givenDate = new Date(message.date);
+let time="";
+if(currentDayOfMonth-message.date[0]==1){
+  time=`yesterday at ${message.date[4]}`
+}
+  if (givenDate.getTime()< firstDayOfCurrentWeek.getTime()) {
+  time=`${message.date[0]}'/'+${message.date[1]}; at ${message.date[4]} `
+}
+if (currentDayOfMonth === message.date[0]) {
+    time=message.date[4];
+}
+else{
+  time=`${message.date[3]} at ${message.date[4]}`  
+}
+
   return (
-    <div className={`message ${message.senderId === currentUser.uid && "owner"}`} >
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`} >
+
       <div className="messageInfo">
         <img
           src={
